08d49d58a2186f1f44cf5e666fbb04fb67048f72
Garen J. Torikian <gjtorikian@users.noreply.github.com>
2019-02-08 01:11:37 +0000

    Fix issues

--- src/index.ts
+++ src/index.ts
@@ -1,97 +1,89 @@
 #! /usr/bin/env node
 
-const ArgumentParser = require("argparse").ArgumentParser;
-const request = require("request");
-const fs = require("fs");
-const path = require("path");
-const tmp = require("tmp");
-const Canvas = require("canvas");
-const Image = Canvas.Image;
+const ArgumentParser = require('argparse').ArgumentParser;
+const request = require('request');
+const fs = require('fs');
+const path = require('path');
+const tmp = require('tmp');
+const {createCanvas, Image} = require('canvas');
 
 let tmpImg = tmp.fileSync();
 let tmpImagePath = tmpImg.name;
 
-let parser = new ArgumentParser({
-  addHelp: true,
-  description: "OOO maker"
-});
+let parser = new ArgumentParser({addHelp: true, description: 'OOO maker'});
 parser.addArgument(
-  [ "-u", "--username" ],
-  {
-    help: "Your GitHub username",
-    required: true
-  }
-);
+    ['-u', '--username'], {help: 'Your GitHub username', required: true});
 parser.addArgument(
-  [ "-d", "--date" ],
-  {
-    help: "The date in a month/day format",
-    required: true
-  }
-);
+    ['-d', '--date'], {help: 'The date in a month/day format', required: true});
 let args = parser.parseArgs();
-let username = args["username"];
-let date = args["date"];
+let username = args['username'];
+let date = args['date'];
 
 request.get(`http://www.github.com/${username}.png`)
-       .on("error", function(error) {
-         console.error("Something went wrong!");
-         throw new error;
-       })
-       .on("response", function(response) {
-         if (response.statusCode !== 200) {
-           throw new TypeError(`Couldn't find a GitHub user called ${username}`);
-         }
-       })
-       .pipe(fs.createWriteStream(tmpImagePath))
-       .on("finish", function() {
-         fs.readFile(tmpImagePath, function(err, avatar) {
-           if (err) throw err;
-           let img = new Image();
-           img.src = avatar;
-           let width = img.width;
-           let height = img.height;
+    .on('error',
+        (error) => {
+          console.error('Something went wrong!');
+          throw new Error(error);
+        })
+    .on('response',
+        (response) => {
+          if (response.statusCode !== 200) {
+            throw new TypeError(
+                `Couldn't find a GitHub user called ${username}`);
+          }
+        })
+    .pipe(fs.createWriteStream(tmpImagePath))
+    .on('finish', () => {
+      fs.readFile(tmpImagePath, (err, avatar) => {
+        if (err) throw err;
+        const img = new Image();
+        img.src = avatar;
+        const width = img.width;
+        const height = img.height;
 
-           let canvas = Canvas.createCanvas(width, height);
-           let ctx = canvas.getContext("2d");
-           ctx.globalAlpha = 0.6;
-           ctx.drawImage(img, 0, 0);
+        const canvas = createCanvas(width, height);
+        const ctx = canvas.getContext('2d');
+        ctx.globalAlpha = 0.6;
+        ctx.drawImage(img, 0, 0);
 
-           ctx.strokeStyle = "black";
-           ctx.fillStyle = "black";
-           ctx.fillRect(0, 0, width, height);
+        ctx.strokeStyle = 'black';
+        ctx.fillStyle = 'black';
+        ctx.fillRect(0, 0, width, height);
 
-           ctx.globalAlpha = 1;
-           ctx.font = "160px Helvetica";
-           ctx.fillStyle = "rgba(255, 255, 255, 1)";
+        ctx.globalAlpha = 1;
+        ctx.font = '160px Helvetica';
+        ctx.fillStyle = 'rgba(255, 255, 255, 1)';
 
-           ctx.fillText("OOO", 50, 130);
-           ctx.fillText("until", 85, 270);
+        ctx.fillText('OOO', 50, 130);
+        ctx.fillText('until', 85, 270);
 
-           let dateLen = date.length;
-           let xPos = 0;
+        const dateLen = date.length;
+        let xPos = 0;
 
-           switch (dateLen) {
-             case 3: // like, 3/6
-              xPos = 120;
-              break;
-             case 4: // like, 7/22
-              xPos = 80;
-              break;
-             case 5: // like, 12/25 or 2/Jan
-              xPos = 40;
-              break;
-             case 6: // like, 14/Dec
-              ctx.font = "130px Helvetica";
-              xPos = 40;
-              break;
-           }
-           ctx.fillText(date, xPos, 430);
+        switch (dateLen) {
+          case 3:  // like, 3/6
+            xPos = 120;
+            break;
+          case 4:  // like, 7/22
+            xPos = 80;
+            break;
+          case 5:  // like, 12/25 or 2/Jan
+            xPos = 40;
+            break;
+          case 6:  // like, 14/Dec
+            ctx.font = '130px Helvetica';
+            xPos = 40;
+            break;
+          default:
+            throw new SyntaxError(`Date ${date} is unrecognized`);
+        }
+        ctx.fillText(date, xPos, 430);
 
-           let out = fs.createWriteStream(path.join(process.cwd(), `${username}.png`));
+        const out =
+            fs.createWriteStream(path.join(process.cwd(), `${username}.png`));
 
-          //  ctx.drawImage(img, 0, 0);
+        //  ctx.drawImage(img, 0, 0);
 
-           canvas.pngStream().pipe(out);
-         });
-       });
+        canvas.pngStream().pipe(out);
+      });
+    });
